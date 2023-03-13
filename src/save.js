103e86b5c028587d346cea7b3b904af346ada9eb
vilu <vilu85@users.noreply.github.com>
2023-03-13 04:03:02 +0200

    - Add edit and save functionalities for the block - Add a view script - Update readme
    Signed-off-by: vilu <vilu85@users.noreply.github.com>

---
+++ src/save.js
@@ -0,0 +1,98 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Saves the block and its attributes.
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function save( props ) {
+	const { attributes } = props;
+	const {
+		minWidth,
+		width,
+		maxWidth,
+		minHeight,
+		height,
+		maxHeight,
+		customCss,
+		border,
+		position,
+		modalSolidBg,
+		modalGradientBg,
+		overlaySolidBg,
+		overlayGradientBg,
+		closeBtnSize,
+		closeBtnColor,
+		closeBtnVariant
+	} = attributes;
+
+	const isCloseBtnVisible = true;	//TODO: Move to attributes
+	const isOpenBtnVisible = true; //TODO: Move to attributes
+
+	const closeBtnStyle = {
+		'--closebtnsize': `${closeBtnSize}px`,
+		color: closeBtnColor
+	}
+
+	const modalStyle = {
+		minWidth,
+		width,
+		maxWidth,
+		minHeight,
+		height,
+		maxHeight,
+		backgroundColor: modalSolidBg,
+		background: modalGradientBg,
+		border
+	}
+
+	const overlayStyle = {
+		backgroundColor: overlaySolidBg,
+		background: overlayGradientBg
+	}
+
+	let sanitizedCustomCss;
+
+	if (customCss && customCss.trim() !== '') {
+		sanitizedCustomCss = customCss.replace(/(<([^>]+)>)/gi, '');
+	}
+
+	return (
+		<div
+			className="epb-popup"
+			data-isopen="false"
+		>
+			{isOpenBtnVisible && (
+				<input type="button" value={__('Show Popup', 'epb-popup')} { ...useBlockProps.save() } />
+			)}
+			<div className="epb-popup-wrapper">
+				<div className="epb-modal-wrapper" data-position={ position } >
+					{sanitizedCustomCss && <style>{sanitizedCustomCss}</style>}
+					<div
+						role="presentation"
+						className="epb-modal-overlay"
+						style={ overlayStyle }
+					/>
+					<div className="epb-modal-content" style={modalStyle}>
+						{isCloseBtnVisible && (
+							<button
+								className="epb-popup-close-btn"
+								style={closeBtnStyle}
+								data-closebtn-variant={ closeBtnVariant }
+							>
+								<span>✕</span>
+							</button>
+						)}
+						<div className="epb-modal-body">
+							<InnerBlocks.Content />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/view.js
@@ -0,0 +1,45 @@
+import domReady from '@wordpress/dom-ready';
+
+domReady(() => {
+	class PopupListeners {
+		constructor(element) {
+			this.element = element;
+			this.hasOpened = false;
+			this.closeBtn = element.querySelector('.epb-modal-content > button');
+			this.overlay = element.querySelector('.epb-modal-overlay');
+
+			document.addEventListener('click', this.handleDocumentClick);
+		}
+
+		handleDocumentClick = (event) => {
+			if (event.target.matches('.epb-popup input[type="button"]')) {
+				event.preventDefault();
+				this.openPopup();
+			} else if (
+				event.target.closest('.epb-popup-close-btn') !== null ||
+				event.target === this.overlay
+			) {
+				this.closePopup();
+			}
+		};
+
+		openPopup() {
+			this.element.dataset.isopen = true;
+			this.hasOpened = true;
+		}
+
+		closePopup() {
+			this.element.dataset.isopen = false;
+		}
+	}
+
+	const popups = document.querySelectorAll('.epb-popup');
+
+	if (!popups.length) {
+		return;
+	}
+
+	popups.forEach((block) => {
+		new PopupListeners(block);
+	});
+});
